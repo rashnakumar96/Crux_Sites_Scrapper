be6bc4be8040674e9dbaf01b2900e1bbc3aad93f
Rashna Kumar <31850566+rashnakumar96@users.noreply.github.com>
2025-08-25 18:51:50 -0500

    Update collect_resources.sh

--- collect_resources.sh
+++ collect_resources.sh
@@ -7,5 +7,5 @@ fi
 country="$1"
 echo "Running HAR capture for $country"
 VPN_COUNTRY=$country \
-NORDVPN_TOKEN="e9f2ab7c6187216b5261e064f6658e697ffd4e8b119bca022a6a97b565e8a228" \
+NORDVPN_TOKEN="" \
 docker-compose up --build --force-recreate --abort-on-container-exit
